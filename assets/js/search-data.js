5a1b02b50fbb3ccf2cad20a3335d21964bf8f288
Epon-Wang <149034652+Epon-Wang@users.noreply.github.com>
2025-10-28 22:35:49 +0000

    Deploying to gh-pages from @ Epon-Wang/epon-wang.github.io@4961db23097f6d941f874ebd051000f765a7fa7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -180,51 +180,36 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-apriltag-positioning-system",
+          title: 'AprilTag Positioning System',
+          description: "Vision Setup for real world experiment of a mobile object rearrangement task",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/AprilTagTracking/";
+            },},{id: "projects-object-rearrangement-with-egocentric-view",
+          title: 'Object Rearrangement with Egocentric View',
+          description: "Action Distillation (DAgger) of nonprehensile manipulation (car pushing a box)",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/activePerception/";
             },},{id: "projects-box-stacking-in-pybullet",
           title: 'Box Stacking in PyBullet',
           description: "Course Projects of ROB-UY 2004",
           section: "Projects",handler: () => {
               window.location.href = "/projects/boxStacking/";
+            },},{id: "projects-e20-simulator",
+          title: 'E20 Simulator',
+          description: "Implementation of an hypothetical 16-bit processor using RISC Architecture with C++",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/e20Simulator/";
+            },},{id: "projects-3d-gesture-based-unlock-system",
+          title: '3D Gesture-Based Unlock System',
+          description: "Course Final Project of ECE-UY 4144",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/gestureUnlock/";
+            },},{id: "projects-spacedoggy-quadruped-locomotion-under-low-gravity",
+          title: 'SpaceDoggy - Quadruped Locomotion Under Low Gravity',
+          description: "Course Term Project of ROB-GY 7863A",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/spaceDoggy/";
             },},{
         id: 'social-email',
         title: 'email',
