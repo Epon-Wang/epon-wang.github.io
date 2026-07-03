501e81fa47a7efbb776a081bf11e8762b33297e8
Epon-Wang <149034652+Epon-Wang@users.noreply.github.com>
2026-07-03 03:32:45 +0000

    Deploying to gh-pages from @ Epon-Wang/epon-wang.github.io@301e8eb4de848964c68400c71a182a910a5e4208 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "Projects",
+  },{id: "nav-blogs",
+          title: "Blogs",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/blog/";
           },
         },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
@@ -27,32 +27,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-acrobatic-2d-quadrotor",
-          title: 'Acrobatic 2D Quadrotor',
-          description: "A Sequential Quadratic Programming (SQP) solver for a 2D quadrotor to perform flipping",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2dQuadrotor/";
-            },},{id: "projects-object-rearrangement-with-egocentric-view",
-          title: 'Object Rearrangement with Egocentric View',
-          description: "Action Distillation (DAgger) of egocentric nonprehensile manipulation (car with FPV camera pushing a box)",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/activePerception/";
-            },},{id: "projects-box-stacking-in-pybullet",
-          title: 'Box Stacking in PyBullet',
-          description: "Impedance controllers for two manipulators to perform box stacking",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/boxStacking/";
-            },},{id: "projects-reinforcement-learning-for-quadrupedal-locomotion",
-          title: 'Reinforcement Learning for Quadrupedal Locomotion',
-          description: "Training robust walking policies for the Unitree Go2 robot using Proximal Policy Optimization (PPO)",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/quadrupedalLocomotion/";
-            },},{id: "projects-quadrupedal-landing-control-under-low-gravity",
-          title: 'Quadrupedal Landing Control Under Low Gravity',
-          description: "Quadrupedal attitude control &amp; landing control under reduced gravity with diverse initial poses, trained using Proximal Policy Optimization (PPO)",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/spaceDoggy/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
