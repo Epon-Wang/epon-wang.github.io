640462a621e332ab3942a761e3332b629bd83ec5
Epon-Wang <149034652+Epon-Wang@users.noreply.github.com>
2026-07-05 19:56:56 +0000

    Deploying to gh-pages from @ Epon-Wang/epon-wang.github.io@2d20d2406128c37657ee12bf4b908d51b8fb5ae6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,6 +42,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/yp-yipeng-wang", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=AUebmUsAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
